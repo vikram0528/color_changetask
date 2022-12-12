dee67fcd540c87bdfeacf38b3761a1b277be0853
vikramlathaa <vikramravi0528@gmail.com>
2022-12-12 20:15:22 +0530

    upload

---
+++ docs/main.js
@@ -0,0 +1,19 @@
+let bttn = document.getElementById("btn");
+let input = document.getElementById("color");
+let text2 = document.getElementById("word")
+
+bttn.addEventListener("click", myFunction);
+
+function myFunction(){
+
+input.value = text2.style.color = input.value;
+
+};
+	
+	
+	
+
+	
+	
+
+
